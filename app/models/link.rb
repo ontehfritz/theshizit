20285888c12dc8eeb3d7de173566d30e0f2a69b3
Fritz Seitz <fritz@theshizit.com>
2011-11-30 22:10:52 -0500

    Added auto detect for youtube videos, embeds link with player

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,27 @@ class Link < Content
 	validates :theshiz, :presence => true
 	validates :title, :presence => true
 	validates_format_of :theshiz, :with => URI::regexp(%w(http https))
+	
+	def is_youtube
+	  if self.theshiz.include? "www.youtube.com"
+	    true
+	  elsif self.theshiz.include? "youtu.be"
+	    true
+	  else
+	    false
+	  end
+	end
+	
+	def youtube_id
+	  tube_id = nil
+  	if self.theshiz[/youtu\.be\/([^\?]*)/]
+      tube_id = $1
+    else
+      self.theshiz[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      tube_id = $5
+    end
+    
+    tube_id
+	end
+	
 end
